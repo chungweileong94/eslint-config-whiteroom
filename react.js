5529e9ed7746d3db21760f0bfdcfa6610235b3c2
Chung Wei Leong <leongchungwei@hotmail.com>
2021-09-05 19:28:36 +0800

    Added react-hook rules

--- react.js
+++ react.js
@@ -1,10 +1,15 @@
 module.exports = {
   extends: ['eslint-config-airbnb-typescript', './lib/share.js'].map(require.resolve),
+  plugins: ['react-hooks'],
   rules: {
     'react/jsx-filename-extension': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/require-default-props': 'off',
     'react/jsx-curly-newline': 'off',
-    'react/jsx-wrap-multilines': 'off'
+    'react/jsx-wrap-multilines': 'off',
+
+    // React Hook
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error'
   }
 };
