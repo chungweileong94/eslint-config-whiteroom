c3826dbe39cc613fd330cfb9e077d65cb2b22130
Chung Wei Leong <15154097+chungweileong94@users.noreply.github.com>
2023-11-07 21:17:53 +0800

    Remove react config (#7)

--- packages/eslint-config-whiteroom/react.js
+++
@@ -1,9 +0,0 @@
-export default {
-  extends: ['./lib/share.js'].map(require.resolve),
-  plugins: ['react-hooks'],
-  rules: {
-    // React Hook
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error'
-  }
-};
